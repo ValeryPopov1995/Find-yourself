047ee05a4f71293e72ded088eddc0b36d908625a
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2020-11-17 23:39:11 +0300

    project created, input system added, post proc added, movenment system added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("Character settings")]
+    public float MoveSpeed = 10f, JumpForce = 10f, Sensitivity = 120f;
+    public VariableJoystick JoystickMove, JoystickView;
+    public Transform CamEmpty;
+
+    bool isGround = false;
+    float xmouse, ymouse;
+    Vector3 moveVector;
+    float yRotation;
+    bool swipeTouched = false;
+
+    void Update()
+    {
+        #region rotation
+        transform.Rotate(Vector3.up * xmouse);
+        yRotation -= ymouse;
+        yRotation = Mathf.Clamp(yRotation, -85f, 60f);
+        CamEmpty.localRotation = Quaternion.Euler(yRotation, 0, 0);
+        #endregion
+
+        #region moving result
+        moveVector = transform.forward * JoystickMove.Vertical * MoveSpeed +
+            transform.right * JoystickMove.Horizontal * MoveSpeed;
+        // rigid body velosity
+        #endregion
+    }
+
+    public void SetSwipeTouched(bool state)
+    {
+        swipeTouched = state;
+    }
+}
