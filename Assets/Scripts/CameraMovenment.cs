f2e7ff9d85005b599482beac36bb69e49ab2830a
ValeryPopov1995 <72905449+Valer-jan@users.noreply.github.com>
2020-11-18 21:44:55 +0300

    camera lerp, stick move & view, swipe added

---
+++ Assets/Scripts/CameraMovenment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovenment : MonoBehaviour
+{
+    [Range(.01f, .5f)]
+    public float MoveLerp, ViewLerp;
+    public Transform StartTarget;
+    public static Transform TargetPosition;
+
+    private void Start()
+    {
+        TargetPosition = StartTarget;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, TargetPosition.position, MoveLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, TargetPosition.rotation, ViewLerp);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,31 +3,65 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [Tooltip("Character settings")]
-    public float MoveSpeed = 10f, JumpForce = 10f, Sensitivity = 120f;
-    public VariableJoystick JoystickMove, JoystickView;
-    public Transform CamEmpty;
+    public enum moveMode { FixedStick, DinamicStick, FloatingStick };
+    public moveMode MoveMode = moveMode.FixedStick;
+    public enum viewMode { FixedStick, DinamicStick, FloatingStick, Swipe };
+    public viewMode ViewMode = viewMode.FixedStick;
+    public GameObject EventTriggerSwipeArea;
+
+    public VariableJoystick MoveStick, ViewStick;
+
+    public int Sensitivity = 100, MoveSpeed = 10;
+    public Transform CameraTarget;
 
     bool isGround = false;
-    float xmouse, ymouse;
-    Vector3 moveVector;
-    float yRotation;
     bool swipeTouched = false;
+    float yRotation;
+    public static Vector3 MoveVector;
+    public static Vector2 ViewVector;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        #region rotation
-        transform.Rotate(Vector3.up * xmouse);
-        yRotation -= ymouse;
-        yRotation = Mathf.Clamp(yRotation, -85f, 60f);
-        CamEmpty.localRotation = Quaternion.Euler(yRotation, 0, 0);
-        #endregion
+        // move
+        rb.velocity = transform.forward * MoveStick.Vertical * MoveSpeed +
+            transform.right * MoveStick.Horizontal * MoveSpeed + 
+            rb.velocity.y * transform.up;
 
-        #region moving result
-        moveVector = transform.forward * JoystickMove.Vertical * MoveSpeed +
-            transform.right * JoystickMove.Horizontal * MoveSpeed;
-        // rigid body velosity
-        #endregion
+        // view
+        if (ViewMode == viewMode.Swipe)
+        {
+            /*if (tch.phase == TouchPhase.Began) swipeStartPos = tch.rawPosition;
+            else if (tch.phase == TouchPhase.Moved)
+            {
+                xmouse = tch.position.x - swipeStartPos.x;
+                ymouse = tch.position.y - swipeStartPos.y;
+
+                xmouse *= Time.deltaTime * Sensitivity / 10;
+                ymouse *= Time.deltaTime * Sensitivity / 10;
+
+                swipeStartPos = tch.position;
+            }
+            else
+            {
+                xmouse = 0f;
+                ymouse = 0f;
+            }*/
+        }
+        else
+        {
+            ViewVector.x = ViewStick.Horizontal * Time.deltaTime * Sensitivity;
+            ViewVector.y = ViewStick.Vertical * Time.deltaTime * Sensitivity;
+        }
+        transform.Rotate(Vector3.up * ViewVector.x);
+        yRotation -= ViewVector.y;
+        yRotation = Mathf.Clamp(yRotation, -85f, 60f);
+        CameraTarget.localRotation = Quaternion.Euler(yRotation, 0, 0);
     }
 
     public void SetSwipeTouched(bool state)
